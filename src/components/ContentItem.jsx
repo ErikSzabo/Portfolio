1953814f670c38979c6967a8de9792ca79d792a5
Erik Szabó <erik1szabo1@gmail.com>
2020-06-29 23:25:26 +0200

    fixed some warnings

--- src/components/ContentItem.jsx
+++ src/components/ContentItem.jsx
@@ -1,10 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
 const ContentItem = ({ title, children }) => {
 	return (
 		<div>
 			<h2 className="p-title">{title}</h2>
-			<p className="p-description">{children}</p>
+			<div className="p-description">{children}</div>
 		</div>
 	);
 };
