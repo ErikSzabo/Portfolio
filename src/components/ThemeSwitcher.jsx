d6a95a0621bc4f89fd7721f60411484c51fddbfe
Erik Szabó <erik1szabo1@gmail.com>
2020-06-29 12:52:21 +0200

    reworked themeswitcher lifecycle

--- src/components/ThemeSwitcher.jsx
+++ src/components/ThemeSwitcher.jsx
@@ -4,32 +4,33 @@ import React, { useState, useEffect } from 'react';
  * CSS Source https://logotar.schdesign.hu/
  */
 
+const LOCAL_STORAGE_KEY = 'ThemeSwitcher.theme';
+
+const themeMap = {
+	dark: 'light',
+	light: 'dark'
+};
+
 const ThemeSwitcher = () => {
 	const [ theme, setTheme ] = useState('dark');
 
 	useEffect(() => {
-		if (!localStorage.getItem('theme')) {
-			localStorage.setItem('theme', 'dark');
-		} else {
-			setTheme(localStorage.getItem('theme'));
+		const possibleTheme = localStorage.getItem(LOCAL_STORAGE_KEY);
+		if (!possibleTheme) return;
+		setTheme(possibleTheme);
+	}, []);
+
+	useEffect(
+		() => {
+			localStorage.setItem(LOCAL_STORAGE_KEY, theme);
 			document.querySelector('body').className = theme;
-		}
-	});
-
-	const themeMap = {
-		dark: 'light',
-		light: 'dark'
-	};
-
-	const switchTheme = () => {
-		document.querySelector('body').className = themeMap[theme];
-		localStorage.setItem('theme', themeMap[theme]);
-		setTheme(themeMap[theme]);
-	};
+		},
+		[ theme ]
+	);
 
 	return (
 		<label className="switch">
-			<input type="checkbox" checked={theme === 'dark'} onChange={switchTheme} />
+			<input type="checkbox" checked={theme === 'dark'} onChange={() => setTheme(themeMap[theme])} />
 			<div className="slider" />
 		</label>
 	);
