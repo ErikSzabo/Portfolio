a78f69641448c802a9ac718296c016e0c30a5b3f
Erik Szabó <erik1szabo1@gmail.com>
2020-06-30 14:18:16 +0200

    still trying to add routing support

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -16,14 +16,14 @@ const Nav = () => (
 				<NavItem name="Home" faIcon="fa fa-home fa-2x" />
 			</Link>
 
-			<Link className="nav-item" to="/projects">
+			<Link className="nav-item" to="/projects/">
 				<NavItem name="Projects" faIcon="fa fa-code fa-2x" />
 			</Link>
 
-			<Link className="nav-item" to="/skills">
+			<Link className="nav-item" to="/skills/">
 				<NavItem name="Skills" faIcon="fa fa-check-square fa-2x" />
 			</Link>
-			<Link className="nav-item" to="/contact">
+			<Link className="nav-item" to="/contact/">
 				<NavItem name="Contact" faIcon="fa fa-envelope fa-2x" />
 			</Link>
 		</ul>
