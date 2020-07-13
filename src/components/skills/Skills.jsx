927d13ebbaa7cbfd0131cc4b3df2a141706db6aa
Erik Szabó <erik1szabo1@gmail.com>
2020-07-13 23:25:51 +0200

    adjusted skills page

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -11,19 +11,30 @@ const Skills = () => (
 	<div className="skills-container">
 		<h1>Skills</h1>
 		<div className="skills">
-			<Skill name="JavaScript" fill="90" logo={jsLogo}>
+			<Skill name="JavaScript" fill="95" logo={jsLogo}>
 				This was my really first language that I learnt. I have really strong emotions about this language, both
 				good and bad, for reasons. I used it first with <span className="text-highlight">static sites</span>,
 				then I moved to <span className="text-highlight">electron</span>, and now, here I am, using{' '}
 				<span className="text-highlight">react</span>.
 			</Skill>
 
-			<Skill name="CSS" fill="60" logo={cssLogo}>
+			<Skill name="CSS" fill="70" logo={cssLogo}>
 				This amazing style language... Well, I need this everywhere. It's my biggest enemy, when it comes to
 				websites. But... everyone can improve over time{' '}
 				<span role="img" aria-label="">
 					😃
 				</span>
+			</Skill>
+
+			<Skill name="Git, GitHub" fill="60" logo={gitLogo}>
+				I've always used git with almost all of my projects. I'm familiar with the{' '}
+				<span className="text-highlight">basic commands</span> and with{' '}
+				<span className="text-highlight">branches</span>, however I haven't really done merges and rebase.
+			</Skill>
+
+			<Skill name="React" fill="60" logo={reactLogo}>
+				Ever since I started web development I've always wanted to try{' '}
+				<span className="text-highlight">react</span>. Now I'm really using it, and it's amazing!
 			</Skill>
 
 			<Skill name="Python" fill="50" logo={pyLogo}>
@@ -35,17 +46,6 @@ const Skills = () => (
 			<Skill name="Java" fill="40" logo={javaLogo}>
 				Java is like fresh air with its static typing after python and other dynamic languages. I really loved
 				it, although I just only used it in the 2nd semester.
-			</Skill>
-
-			<Skill name="Git, GitHub" fill="60" logo={gitLogo}>
-				I've always used git with almost all of my projects. I'm familiar with the{' '}
-				<span className="text-highlight">basic commands</span> and with{' '}
-				<span className="text-highlight">branches</span>, however I haven't really done merges and rebase.
-			</Skill>
-
-			<Skill name="React" fill="50" logo={reactLogo}>
-				Ever since I started web development I've always wanted to try{' '}
-				<span className="text-highlight">react</span>. Now I'm really using it, and it's amazing!
 			</Skill>
 		</div>
 	</div>
