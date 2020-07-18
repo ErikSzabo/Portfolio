9c3185255b9fd673d2f32bac85b6cda8fd88fb3f
Erik Szabó <erik1szabo1@gmail.com>
2020-07-18 13:09:34 +0200

    Skills now using the API

--- src/components/skills/Skill.jsx
+++ src/components/skills/Skill.jsx
@@ -1,18 +1,21 @@
 import React from 'react';
 
-const Skill = ({ name, logo, fill, children }) => (
-	<div className="skill">
-		<div className="skill-name">
-			<img src={logo} alt="" />
-			{name}
-		</div>
-		<div className="skill-short">{children}</div>
-		<div className="progress-bar">
-			<div className="progress-bar-bg">
-				<div className="progress-bar-front" style={{ width: `${fill}%` }} />
-			</div>
-		</div>
-	</div>
+const Skill = ({ name, logo, fill, description }) => (
+    <div className="skill">
+        <div className="skill-name">
+            <img src={logo} alt="" />
+            {name}
+        </div>
+        <div className="skill-short">{description}</div>
+        <div className="progress-bar">
+            <div className="progress-bar-bg">
+                <div
+                    className="progress-bar-front"
+                    style={{ width: `${fill}%` }}
+                />
+            </div>
+        </div>
+    </div>
 );
 
 export default Skill;
--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,54 +1,34 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Skill from './Skill';
-import jsLogo from '../../images/js_logo.png';
-import cssLogo from '../../images/css_logo.png';
-import pyLogo from '../../images/py_logo.png';
-import javaLogo from '../../images/java_logo.png';
-import gitLogo from '../../images/git_logo.png';
-import reactLogo from '../../images/react_logo.png';
 
-const Skills = () => (
-	<div className="skills-container">
-		<h1>Skills</h1>
-		<div className="skills">
-			<Skill name="JavaScript" fill="95" logo={jsLogo}>
-				This was my really first language that I learnt. I have really strong emotions about this language, both
-				good and bad, for reasons. I used it first with <span className="text-highlight">static sites</span>,
-				then I moved to <span className="text-highlight">electron</span>, and now, here I am, using{' '}
-				<span className="text-highlight">react</span>.
-			</Skill>
+const Skills = () => {
+    const [skills, setSkills] = useState([]);
 
-			<Skill name="CSS" fill="70" logo={cssLogo}>
-				This amazing style language... Well, I need this everywhere. It's my biggest enemy, when it comes to
-				websites. But... everyone can improve over time{' '}
-				<span role="img" aria-label="">
-					😃
-				</span>
-			</Skill>
+    useEffect(() => {
+        (async () => {
+            const raw = await fetch(
+                'https://devrik-net.herokuapp.com/api/v1/skills'
+            );
+            const result = await raw.json();
+            setSkills(result);
+        })();
+    }, []);
 
-			<Skill name="Git, GitHub" fill="60" logo={gitLogo}>
-				I've always used git with almost all of my projects. I'm familiar with the{' '}
-				<span className="text-highlight">basic commands</span> and with{' '}
-				<span className="text-highlight">branches</span>, however I haven't really done merges and rebase.
-			</Skill>
-
-			<Skill name="React" fill="60" logo={reactLogo}>
-				Ever since I started web development I've always wanted to try{' '}
-				<span className="text-highlight">react</span>. Now I'm really using it, and it's amazing!
-			</Skill>
-
-			<Skill name="Python" fill="50" logo={pyLogo}>
-				There aren't any developer who didn't tried python at least once. But this time it's different. I fell
-				in love with <span className="text-highlight">pygame</span> and especially with the{' '}
-				<span className="text-highlight">sockets</span> module.
-			</Skill>
-
-			<Skill name="Java" fill="40" logo={javaLogo}>
-				Java is like fresh air with its static typing after python and other dynamic languages. I really loved
-				it, although I just only used it in the 2nd semester.
-			</Skill>
-		</div>
-	</div>
-);
+    return (
+        <div className="skills-container">
+            <h1>Skills</h1>
+            <div className="skills">
+                {skills.map((skill) => (
+                    <Skill
+                        name={skill.name}
+                        fill={skill.progress}
+                        description={skill.description}
+                        logo={skill.icon}
+                    />
+                ))}
+            </div>
+        </div>
+    );
+};
 
 export default Skills;
