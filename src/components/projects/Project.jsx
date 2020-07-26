dc17d913a2b508e46f59f6103b9c3366168062ab
Erik Szabó <erik1szabo1@gmail.com>
2020-07-26 20:17:26 +0200

    #G2 Project/Skill components only visible when imgs loaded

--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -1,22 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Tag from './Tag';
 
-const Project = ({ id, name, img, description, tags }) => (
-    <Link to={`/projects/${id}`}>
-        <div className="project-item">
-            <img src={img} alt="" />
-            <div className="project-text">
-                <h3>{name}</h3>
-                <p>{description}</p>
-                <div className="project-tags">
-                    {tags.map((tag, index) => {
-                        return <Tag key={index} name={tag} />;
-                    })}
+const Project = ({ id, name, img, description, tags }) => {
+    const [imgLoaded, setImgLoaded] = useState(false);
+
+    return (
+        <Link to={`/projects/${id}`} className={!imgLoaded && 'hidden'}>
+            <div className="project-item">
+                <img onLoad={() => setImgLoaded(true)} src={img} alt="" />
+                <div className="project-text">
+                    <h3>{name}</h3>
+                    <p>{description}</p>
+                    <div className="project-tags">
+                        {tags.map((tag, index) => {
+                            return <Tag key={index} name={tag} />;
+                        })}
+                    </div>
                 </div>
             </div>
-        </div>
-    </Link>
-);
+        </Link>
+    );
+};
 
 export default Project;
--- src/components/skills/Skill.jsx
+++ src/components/skills/Skill.jsx
@@ -1,21 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Skill = ({ name, logo, fill, description }) => (
-    <div className="skill">
-        <div className="skill-name">
-            <img src={logo} alt="" />
-            {name}
-        </div>
-        <div className="skill-short">{description}</div>
-        <div className="progress-bar">
-            <div className="progress-bar-bg">
-                <div
-                    className="progress-bar-front"
-                    style={{ width: `${fill}%` }}
-                />
+const Skill = ({ name, logo, fill, description }) => {
+    const [imgLoaded, setImgLoaded] = useState(false);
+
+    return (
+        <div className={`skill ${!imgLoaded && 'hidden'}`}>
+            <div className="skill-name">
+                <img onLoad={() => setImgLoaded(true)} src={logo} alt="" />
+                {name}
+            </div>
+            <div className="skill-short">{description}</div>
+            <div className="progress-bar">
+                <div className="progress-bar-bg">
+                    <div
+                        className="progress-bar-front"
+                        style={{ width: `${fill}%` }}
+                    />
+                </div>
             </div>
         </div>
-    </div>
-);
+    );
+};
 
 export default Skill;
