17e8e1c241784ebf6652eba9528f719453f0b138
Erik Szabó <erik1szabo1@gmail.com>
2020-07-28 19:42:10 +0200

    Added basic frontpage

--- src/components/landing/Landing.jsx
+++ src/components/landing/Landing.jsx
@@ -1,15 +1,46 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import github from '../../assets/github.png';
+import './Landing.scoped.css';
 
-const Landing = () => (
-	<div>
-		<h1 style={{ fontSize: '26px' }}>Under Development!</h1>
-		<p>
-			Design partly insipired by{' '}
-			<a className="text-highlight" href="https://fireship.io" target="_blank" rel="noopener noreferrer">
-				fireship.io
-			</a>
-		</p>
-	</div>
-);
+const Landing = () => {
+    return (
+        <div className="landing-page">
+            <div className="landing">
+                <h1 className="landing__title">Hey, my name is Erik Szabó!</h1>
+                <p className="landing__description">
+                    I'm an unversity student in Hungary at{' '}
+                    <span className="text-highlight">
+                        Budapest University of Technology and Economcs
+                    </span>
+                    . I want to become a full stack developer, but until then,
+                    you can check out my projects and things that I'am currently
+                    good at. If you are intrested in any ways, feel free to
+                    contact me!
+                </p>
+                <div className="landing__buttons">
+                    <Link to="/projects">
+                        <div className="landing__btn">{'<Projects />'}</div>
+                    </Link>
+                    <a
+                        href="https://github.com/ErikSzabo?tab=repositories"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        <div className="landing__btn--git">
+                            <img
+                                src={github}
+                                alt=""
+                                className="landing__git"
+                            ></img>
+                            Github
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <div className="development hidden">Under Development!</div>
+        </div>
+    );
+};
 
 export default Landing;
