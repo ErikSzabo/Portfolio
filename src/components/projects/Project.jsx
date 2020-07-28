a4bc5df2f435cd4d12bde4c70fa8503484c46342
Erik Szabó <erik1szabo1@gmail.com>
2020-07-28 13:37:06 +0200

    fade in animation to projects and skills

--- src/components/projects/Project.jsx
+++ src/components/projects/Project.jsx
@@ -1,13 +1,22 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import Tag from './Tag';
+import useWebAnimations, { fadeIn } from '@wellyshen/use-web-animations';
 import './Project.css';
 
 const Project = ({ id, name, img, description, tags }) => {
     const [imgLoaded, setImgLoaded] = useState(false);
+    const { ref } = useWebAnimations({
+        ...fadeIn,
+        timing: { duration: 500 },
+    });
 
     return (
-        <Link to={`/projects/${id}`} className={!imgLoaded && 'hidden'}>
+        <Link
+            to={`/projects/${id}`}
+            className={!imgLoaded && 'hidden'}
+            ref={ref}
+        >
             <div className="project-item">
                 <img onLoad={() => setImgLoaded(true)} src={img} alt="" />
                 <div className="project-text">
--- src/components/skills/Skill.jsx
+++ src/components/skills/Skill.jsx
@@ -1,11 +1,16 @@
 import React, { useState } from 'react';
+import useWebAnimations, { fadeIn } from '@wellyshen/use-web-animations';
 import './Skill.css';
 
 const Skill = ({ name, logo, fill, description }) => {
     const [imgLoaded, setImgLoaded] = useState(false);
+    const { ref } = useWebAnimations({
+        ...fadeIn,
+        timing: { duration: 500 },
+    });
 
     return (
-        <div className={`skill ${!imgLoaded && 'hidden'}`}>
+        <div className={`skill ${!imgLoaded && 'hidden'}`} ref={ref}>
             <div className="skill-name">
                 <img onLoad={() => setImgLoaded(true)} src={logo} alt="" />
                 {name}
